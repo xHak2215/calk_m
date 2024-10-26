245c2a2bd290dfc6db6a989a994b837129f85b75
xHak2215 <147624393+xHak2215@users.noreply.github.com>
2024-10-26 19:57:21 +0300

    Delete calk_m.py directory

--- calk_m.py/__init__.py
+++
@@ -1,1 +0,0 @@
-from calk_m import *
--- calk_m.py/setup.py
+++
@@ -1,5 +0,0 @@
-import os
-mein_direct=str(os.path.dirname(os.path.abspath(__name__)))
-mein_direct.encode("utf-8")
-os.chdir(mein_direct)
-os.system("pip install -r requirements.txt")
